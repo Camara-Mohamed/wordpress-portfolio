b9c379d3bea09972246fe8206980aed77d98152c
Camara-Mohamed <mohamed.camara1@student.hepl.be>
2025-05-21 12:02:16 +0200

    feat(page): build legal page with microdata

--- wp-content/themes/portfolio/template-legales.php
+++ wp-content/themes/portfolio/template-legales.php
@@ -9,6 +9,26 @@
     <main id="main-content" class="main-content" role="main">
         <?php get_template_part('templates/partials/section-hero'); ?>
 
+        <section class="legals" aria-labelledby="legal-title" itemscope itemtype="https://schema.org/WebPage">
+            <div class="legals__container" itemprop="mainContentOfPage">
+                <h2 id="legal-title" class="legal__title hidden" itemprop="headline">Mentions légales</h2>
+
+                <?php if (have_rows('sections_mentions')) : ?>
+                    <div class="legals__content" itemscope itemtype="https://schema.org/ItemList">
+                        <?php while (have_rows('sections_mentions')) : the_row(); ?>
+                            <article class="legals__content--paragraph" itemprop="itemListElement" itemscope
+                                     itemtype="https://schema.org/ListItem">
+                                <h3 class="legals__paragraph--title"
+                                    itemprop="name"><?= get_sub_field('title_legals') ?></h3>
+                                <div class="legals__paragraph--content" itemprop="description">
+                                    <?= get_sub_field('content_legals') ?>
+                                </div>
+                            </article>
+                        <?php endwhile; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </section>
     </main>
 
 <?php get_footer(); ?>
