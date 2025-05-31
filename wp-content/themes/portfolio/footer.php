73f8b483929d8d6656286922a4953e91ea94c04a
Camara-Mohamed <mohamed.camara1@student.hepl.be>
2025-05-31 15:37:43 +0200

    Stylizing footer and arrange content

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -3,11 +3,29 @@
         <h2 class="sro" aria-level="2"><?php _e('Navigation de pied de page', 'portfolio-detective'); ?></h2>
         <section class="footer__section">
             <h3 class="sro"><?php _e('Mes informations', 'portfolio-detective'); ?></h3>
-            <p class="footer__name">Mohamed Camara</p>
+            <a class="footer__title" href="<?= home_url('/'); ?>" itemprop="url"
+               title="<?php _e('Aller à la page d\'accueil');
+               ?>">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
+                    <g clip-path="url(#clip0_487_894)">
+                        <path d="M9.19365 20C9.19365 14.0906 13.9306 9.2996 19.7735 9.2996V0C8.85301 0 0 8.95389 0 20C0 31.0461 8.85301 40 19.7747 40V30.7016C13.9318 30.7016 9.19483 25.9106 9.19483 20.0012L9.19365 20Z"
+                              fill="#2A2118"/>
+                        <path d="M40 40V28.8426V19.5442H30.8052H19.7747V28.8426H30.8052V40H40Z" fill="#C1272D"/>
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_487_894">
+                            <rect width="40" height="40" fill="white"/>
+                        </clipPath>
+                    </defs>
+                </svg>
+                Mohamed Camara
+            </a>
             <div class="footer__contact">
-                <h4 class="footer__contact--title"><?php _e('Contactez-moi', 'portfolio-detective'); ?></h4>
-                <p class="footer__contact--content"><?php _e('Mon profil vous intéresse ?',
+                <div class="footer__contact--content">
+                <h4 class="title"><?php _e('Contactez-moi', 'portfolio-detective'); ?></h4>
+                    <p><?php _e('Mon profil vous intéresse ?',
                         'portfolio-detective'); ?></p>
+                </div>
                 <a href="<?= dw_translated_url('/me-contacter'); ?>" class="footer__contact--button"
                    aria-label="<?php _e('Aller à la page de contact', 'portfolio-detective'); ?>"
                    title="<?php _e('Aller à la page de contact', 'portfolio-detective'); ?>">
@@ -40,7 +58,7 @@
             ?>
         </aside>
 
-        <aside class="footer__section">
+        <aside class="footer__aside">
             <h3 class="footer__sociales--title"><?php _e('Réseaux', 'portfolio-detective'); ?></h3>
             <?php
             wp_nav_menu([
