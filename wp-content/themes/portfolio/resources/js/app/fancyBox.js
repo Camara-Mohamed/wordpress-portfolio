804d7168c2f9c47052350f351159990b68975028
Camara-Mohamed <mohamed.camara1@student.hepl.be>
2025-06-08 00:31:04 +0200

    style(about): style section, add images, remove fancybox

--- wp-content/themes/portfolio/resources/js/app/fancyBox.js
+++
@@ -1,7 +0,0 @@
-import {Fancybox} from "@fancyapps/ui";
-
-document.addEventListener("DOMContentLoaded", function () {
-    Fancybox.bind('[data-fancybox="gallery"]', {
-
-    });
-});
--- wp-content/themes/portfolio/resources/js/main.js
+++ wp-content/themes/portfolio/resources/js/main.js
@@ -1,9 +1,2 @@
 
 
-import {Fancybox} from "@fancyapps/ui";
-
-document.addEventListener("DOMContentLoaded", function () {
-    Fancybox.bind('[data-fancybox="gallery"]', {
-
-    });
-});
